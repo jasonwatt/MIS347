c1a2b354c745db14bdf720f92af2e814d801f711
Siddharth Singh <888.siddharthsingh@Gmail.com>
2016-08-11 17:08:40 +0530

    fixed profile_exp.php

--- html/patron/profile_exp.php
+++ html/patron/profile_exp.php
@@ -2,16 +2,28 @@
 define('DB_USER', '');
 define('DB_PASSWORD', '');
 define('DB_HOST', '');
-    $conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,'skecomplaints'); // $config['username'], $config['password'],
+define('DB_NAME', 'skecomplaints');
+    $conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);	// $config['username'], $config['password'],
    
+    if(! $conn)
+{
+	die('Could not connect');
+}
+
+$db_selected = mysql_select_db(DB_NAME, $conn);
+
+
+	if(! $db_selected)
+	{
+		die('Cannot use ' . DB_NAME . ': ' . mysql_error());
+	}
+
         session_start(); 
   
 
 
     // Check connection
-       if ($conn->connect_error) {
-           die("Connection failed: " . $conn->connect_error);
-       } 
+       
     try {
 		
     $User_ID= $_SESSION["sessionUserID"]; 
@@ -20,8 +32,8 @@ define('DB_HOST', '');
     }
 
     $sql = "SELECT User_ID, Username, Profile_Pic, Email, User_Type FROM user WHERE User_ID= ".$User_ID." ";
-    $result = $conn->query($sql);
-    while($row = $result->fetch_assoc()) {            
+    $result = mysql_query($sql,$conn);
+    while($row = mysql_fetch_assoc($result)) {            
     $User_ID= $row["User_ID"];
     $Username= $row["Username"];
     $User_Type= $row["User_Type"];
@@ -71,7 +83,7 @@ define('DB_HOST', '');
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href="sass.html">Home</a></li>
-                    <li><a href="logout.php">Logout</a></li>
+                    <li><a href="../../php/logout.php">Logout</a></li>
                 </ul>
                 <ul class="side-nav" id="mobile-demo">
                     <li><a href="#">Profile</a></li>
@@ -95,7 +107,7 @@ define('DB_HOST', '');
                     <h6><?php echo "$Email"; ?></h6>
                 </div>
 
-                <a class="btn-floating btn-large waves-effect waves-light red right" id="pp_fab"><i class="material-icons">mode_edit</i></a>
+                <a class="btn-floating btn-large waves-effect waves-light red right" id="pp_fab" href="../shared/edit_profile.html"><i class="material-icons">mode_edit</i></a>
 
             </form>
         </div>
