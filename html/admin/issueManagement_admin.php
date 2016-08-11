35339173b3533ae7cfb65dd698e52685a7f946c0
rohansampson <rohansampson@yahoo.co.in>
2016-08-11 21:20:57 +0530

    db access fixes

--- html/admin/issueManagement_admin.php
+++ html/admin/issueManagement_admin.php
@@ -66,9 +66,9 @@
                     <tbody>
                         <?php
 
-                        define('DB_USER', '');
-						define('DB_PASSWORD', '');
-						define('DB_HOST', '');
+                        define('DB_USER', 'ske');
+						define('DB_PASSWORD', 'ske');
+						define('DB_HOST', 'localhost');
 						$conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,'skecomplaints');
                         if(! $conn)
                         {
