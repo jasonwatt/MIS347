047017942c386fad8f6138132d2dd5382b783a1e
Siddharth Singh <888.siddharthsingh@Gmail.com>
2016-08-11 21:12:33 +0530

    issueAdd to add issues from admin

---
+++ php/issueAdd.php
@@ -0,0 +1,37 @@
+<?php
+define('DB_NAME', 'skecomplaints');
+define('DB_USER', 'ske');
+define('DB_PASSWORD', 'ske');
+define('DB_HOST', '');
+
+$conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if(! $conn)
+{
+	die('Could not connect');
+}
+
+$db_selected = mysql_select_db(DB_NAME, $conn);
+
+
+	if(! $db_selected)
+	{
+		die('Cannot use ' . DB_NAME . ': ' . mysql_error());
+	}
+	
+	
+	$issueTitle = $_POST["issueT"];
+	$issueDetail = $_POST["issueD"];
+	$sql = "INSERT INTO issues (Summary, Status ) 
+	VALUES ('$issueTitle','$issueDetail')";
+
+if (mysql_query($sql,$conn) === TRUE) {
+    echo "\n New record created successfully";
+	header("Location: ../html/admin/issueManagement_admin.php");
+} else {
+    echo "Error: " . $sql . "<br>" . mysql_error($conn);
+}
+
+mysql_close($conn);
+
+	
+?>
